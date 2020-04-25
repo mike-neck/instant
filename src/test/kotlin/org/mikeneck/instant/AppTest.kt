db746fb19f413b579f11cc44035675b199640507
mike_neck <jkrt3333@gmail.com>
2020-04-25 12:48:27 +0900

    reformat

--- src/test/kotlin/org/mikeneck/instant/AppTest.kt
+++ src/test/kotlin/org/mikeneck/instant/AppTest.kt
@@ -8,14 +8,14 @@ import run.ktcheck.assertion.MatcherSupport
 import run.ktcheck.assertion.NoDep.should
 
 object EitherAssert {
-  fun <L: Any, R: Any> beRight(): Matcher<Either<L, R>> =
+  fun <L : Any, R : Any> beRight(): Matcher<Either<L, R>> =
       object : MatcherSupport<Either<L, R>>() {
         override val expectedValue: Any = "Either.Right"
 
         override fun matches(actual: Either<L, R>): Boolean = actual is Right
       }
 
-  fun <L: Any, R: Any> beLeft(): Matcher<Either<L, R>> =
+  fun <L : Any, R : Any> beLeft(): Matcher<Either<L, R>> =
       object : MatcherSupport<Either<L, R>>() {
         override val expectedValue: Any = "Either.Left"
 
@@ -25,10 +25,13 @@ object EitherAssert {
 }
 
 @Suppress("RemoveExplicitTypeArguments")
-object AppFormatterUnixTest: KtCheck
+object AppFormatterUnixTest : KtCheck
 by Given("Create App with 'instant -f unix'", { App.ofUnix() })
-    .When("call 'formatter()'", { app -> app.formatter() })
-    .Then("it should be Right", { _, either: Either<String, Formatter> -> 
-      either should beRight<String, Formatter>() })
+    .When("call 'formatter()'", { app ->
+      app.formatter()
+    })
+    .Then("it should be Right", { _, either: Either<String, Formatter> ->
+      either should beRight<String, Formatter>()
+    })
 
 
