b6c5ec2fbfb99d32dd3ad8057cba7c0d9ec3378e
mike_neck <jkrt3333@gmail.com>
2020-04-25 20:35:05 +0900

    add test to showTime

--- src/test/kotlin/org/mikeneck/instant/AppTest.kt
+++ src/test/kotlin/org/mikeneck/instant/AppTest.kt
@@ -1,6 +1,7 @@
 package org.mikeneck.instant
 
 import org.mikeneck.instant.EitherAssert.beLeft
+import org.mikeneck.instant.EitherAssert.beLeftContaining
 import org.mikeneck.instant.EitherAssert.beRight
 import run.ktcheck.Given
 import run.ktcheck.KtCheck
@@ -14,6 +15,7 @@ import java.time.LocalTime
 import java.time.OffsetDateTime
 import java.time.ZoneId
 import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 object EitherAssert {
   fun <L : Any, R : Any> beRight(): Matcher<Either<L, R>> =
@@ -23,6 +25,14 @@ object EitherAssert {
         override fun matches(actual: Either<L, R>): Boolean = actual is Right
       }
 
+  fun beRight(value: String): Matcher<Either<String, String>> =
+      object : MatcherSupport<Either<String, String>>() {
+        override val expectedValue: Any get() = "Right with value[$value]"
+
+        override fun matches(actual: Either<String, String>): Boolean =
+            actual == Either.right<String, String>(value)
+      }
+
   fun <L : Any, R : Any> beLeft(): Matcher<Either<L, R>> =
       object : MatcherSupport<Either<L, R>>() {
         override val expectedValue: Any = "Either.Left"
@@ -30,6 +40,16 @@ object EitherAssert {
         override fun matches(actual: Either<L, R>): Boolean = actual is Left
       }
 
+  fun beLeftContaining(pattern: String): Matcher<Either<String, String>> =
+      object : MatcherSupport<Either<String, String>>() {
+        override val expectedValue: Any = "Left with value containing $pattern" 
+
+        override fun matches(actual: Either<String, String>): Boolean =
+            when (actual) {
+              is Right -> false
+              is Left -> actual.value.contains(pattern)
+            }
+      }
 }
 
 @Suppress("RemoveExplicitTypeArguments")
@@ -75,4 +95,31 @@ by Given(
     .When("call now()", { app -> app.now() })
     .Then("it is the same offsetDateTime as the context", { _, offsetDateTime ->
       offsetDateTime shouldBe this
+    })
+
+object AppShowTimeTest : KtCheck
+by Given(
+    description = "Create App with fixed Instant Clock",
+    before = { OffsetDateTime.of(
+        LocalDate.of(2020, 1, 2),
+        LocalTime.of(15, 4), ZoneOffset.UTC) },
+    action = { App(Clock.fixed(this.toInstant(), ZoneId.of("UTC"))) } 
+)
+    .When("call showTime with unix like formatter", { app -> 
+      app.showTime { "${it.toInstant().toEpochMilli()}" }
+    })
+    .Then("it should Right of integral string", { _, either ->
+      either should beRight("${this.toInstant().toEpochMilli()}")
+    })
+    .When("call showTime with dateTimeFormatter", { app ->
+      app.showTime { DateTimeFormatter.ISO_OFFSET_DATE.format(it) }
+    })
+    .Then("it should be Right of 2020-01-02Z", { _, either ->
+      either should beRight("2020-01-02Z")
+    })
+    .When("call showTime with unavailable pattern", { app ->
+      app.showTime { DateTimeFormatter.ofPattern("z").format(it) }
+    })
+    .Then("it should be Left with message containing 'Unable to extract ZoneId'", { _, either ->
+      either should beLeftContaining("Unable to extract ZoneId")
     })
