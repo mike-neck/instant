bb0e7476d23f737dd2b34a20899daed790068c49
mike_neck <jkrt3333@gmail.com>
2020-05-09 22:10:06 +0900

    return to current branch

--- release.sh
+++ release.sh
@@ -15,8 +15,13 @@ else
     exit 1
 fi
 
+current_branch=$(git branch | grep '\*' | awk '{print $2}')
+
 git fetch
 branch_name=$(instant -f unix)
+
+echo "create new branch: release/${branch_name}"
+
 git checkout -b "release/${branch_name}" origin/master
 
 git commit --allow-empty -m "release/${level}"
@@ -25,3 +30,5 @@ gh pr create \
   --base master \
   --title "release/${level}" \
   --body "release/${level}" 
+
+git checkout "${current_branch}"
