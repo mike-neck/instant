9019b08fa422c03084dbc8706d9fd77975029778
mike_neck <jkrt3333@gmail.com>
2020-05-09 21:54:14 +0900

    script for release

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -eu
+
+level=$1
+
+if [[ "${level}" == "major" ]]; then
+    echo "major release"
+elif [[ "${level}" == "minor" ]]; then
+    echo "minor release"
+elif [[ "${level}" == "patch" ]]; then
+    echo "patch release"
+else
+    echo "./release.sh [major | minor | patch]"
+    exit 1
+fi
+
+git fetch
+branch_name=$(instant -f unix)
+git checkout -b "release/${branch_name}" origin/master
+
+git commit --allow-empty -m "release/${level}"
+
+gh pr create \
+  --target master \
+  --title "release/${branch_name}" \
+  --body "release/${branch_name}" 
